647cf0e3e517d402f264c6195bac3defb564757d
khawla lamsiyeh <61538329+khawlalam@users.noreply.github.com>
2025-05-22 23:44:46 +0000

    add spring security

---
+++ src/main/java/enset_bdcc/bdccensetspringmvc/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package enset_bdcc.bdccensetspringmvc.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password("1234").roles("USER").build(),
+                User.withUsername("user2").password("1234").roles("USER").build(),
+                User.withUsername("admin").password("1234").roles("USER","Admin").build()
+
+                );
+    }
+}
+
